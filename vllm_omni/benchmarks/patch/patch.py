e1bb1fe9cf701fed7dbc1161f050e1177e98cb8b
Dong Wang <dongw2019@gmail.com>
2026-02-18 10:22:55 +0000

    fix CI
    Signed-off-by: Dong Wang <dongw2019@gmail.com>

--- vllm_omni/benchmarks/patch/patch.py
+++ vllm_omni/benchmarks/patch/patch.py
@@ -18,6 +18,7 @@ import aiohttp
 from pydub import AudioSegment
 from tqdm.asyncio import tqdm
 from transformers import PreTrainedTokenizerBase
+from vllm.benchmarks import datasets
 from vllm.benchmarks.datasets import SampleRequest
 from vllm.benchmarks.lib.endpoint_request_func import (
     ASYNC_REQUEST_FUNCS,
@@ -31,8 +32,6 @@ from vllm.benchmarks.lib.endpoint_request_func import (
     _validate_api_url,
 )
 from vllm.logger import init_logger
-
-from vllm.benchmarks import datasets
 
 logger = init_logger(__name__)
 from vllm_omni.benchmarks.data_modules.random_multi_modal_dataset import OmniRandomMultiModalDataset
@@ -205,9 +204,9 @@ if "openai-chat-omni" not in OPENAI_COMPATIBLE_BACKENDS:
 
 # ruff: noqa: E402
 # Prevent import order from causing patch failures
+from vllm.benchmarks import serve
 from vllm.benchmarks.serve import TaskType, calculate_metrics_for_embeddings, get_request, wait_for_endpoint
 
-from vllm.benchmarks import serve
 from vllm_omni.benchmarks.metrics.metrics import MultiModalsBenchmarkMetrics, calculate_metrics
 
 # ruff: noqa: E402
